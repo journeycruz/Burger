cb90b1a97ab5bbfdf17550832e99bbe20f342927
Journey Cruz <journey.cruz@gmail.com>
2020-07-18 14:32:19 -0500

    add jawsdb to connection.js

--- config/connection.js
+++ config/connection.js
@@ -1,19 +1,23 @@
 var mysql = require("mysql");
 
-const connection = mysql.createConnection({
-  host: "127.0.0.1",
-  port: 3306,
-  user: "root",
-  password: "rootPASS7",
-  database: "burgers_db",
-});
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    const connection = mysql.createConnection({
+        host: "127.0.0.1",
+        port: 3306,
+        user: "root",
+        password: "rootPASS7",
+        database: "burgers_db",
+    });
+}
 
-connection.connect(function(err) {
-  if (err) {
-    console.error("error connecting: " + err.stack);
-    return;
-  }
-  console.log("connected as id " + connection.threadId);
+connection.connect(function (err) {
+    if (err) {
+        console.error("error connecting: " + err.stack);
+        return;
+    }
+    console.log("connected as id " + connection.threadId);
 });
 
 module.exports = connection;
--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var express = require("express");
 var path = require("path");
 
-var PORT = process.env.PORT || 3306;
+var PORT = process.env.PORT || 8080;
 
 var app = express();
 
