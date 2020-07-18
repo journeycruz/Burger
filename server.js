7776e9a5a721a8503a2f35a238e9a9b2e745fb9c
Journey Cruz <journey.cruz@gmail.com>
2020-07-18 09:50:06 -0500

    change port in server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var express = require("express");
 var path = require("path");
 
-var PORT = process.env.PORT || 8080;
+var PORT = process.env.PORT || 3000;
 
 var app = express();
 
@@ -26,5 +26,5 @@ app.use(routes);
 // Start our server so that it can begin listening to client requests.
 app.listen(PORT, function() {
   // Log (server-side) when our server has started
-  console.log("Server listening on: http://localhost:" + PORT);
+  console.log(`Server listening on: http://localhost: ${PORT}`);
 });
