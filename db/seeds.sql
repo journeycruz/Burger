d91ade335b03dfbcde64514ed09eb4ef8feb764b
Journey Cruz <journey.cruz@gmail.com>
2020-07-17 20:47:32 -0500

    create database and source the files through command line

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Double-Cheeseburger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Jalapeno Bacon Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Buffalo Chicken Sandwich', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Mushroom Swiss Burger', false);
