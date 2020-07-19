b3435dbabd822622d83e31f16c6acd633edacf50
Journey Cruz <journey.cruz@gmail.com>
2020-07-18 19:52:19 -0500

    add picture, and add styling in index.handlebars

--- config/orm.js
+++ config/orm.js
@@ -19,15 +19,15 @@ var orm = {
             cb(result);
         });
     },
-    updateOne: function (id, cb) {
-        var queryString = "UPDATE burgers SET devoured = true WHERE id = (?)";
-        connection.query(queryString, [id], function (err, result) {
-            if (err) {
-                throw err;
-            }
-            cb(result);
-        })
-    }
+    updateOne: function( id, cb) {
+        var queryString = "UPDATE burgers SET devour = 1 where id = ?";
+        connection.query(queryString, id, function(err, result) {
+          if (err) {
+            throw err;
+          }
+          cb(result);
+        });
+      }
 }
 
 module.exports = orm;
